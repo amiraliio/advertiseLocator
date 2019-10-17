b220bceb11bab748c3d970789c74a754ad000139
amiralii <mr.roshanae@gmail.com>
2019-10-17 10:12:35 +0330

    echo routes added

--- Readme.md
+++ Readme.md
@@ -1,3 +1,2 @@
 # Advertise Locator
 
-
---
+++ controllers/v1/auth.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+
+func PersonRegister(context echo.Context) error{
+
+}
+
+
+
+func PersonLogin(context echo.Context) error{
+
+}
